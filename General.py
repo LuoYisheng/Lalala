94c1a8cd1e61a7bc85f4e952d0dbfcca9f2e1bbe
LuoYisheng <yisheng.luo.2013@mitb.smu.edu.sg>
2014-09-18 21:13:35 +0800

    Update General.py

--- General.py
+++ General.py
@@ -1,4 +1,4 @@
 print ("Hey it's a python script!")
-import pandas as pd
-data = read_csv(https://www.dropbox.com/sh/3aq8c4xt5aivpwc/AADGt13BQDuE6_siKoavrhaOa?dl=0)
-data.describe
+import read_csv from pandas
+data = read_csv(https://dl.dropboxusercontent.com/u/58264487/test.csv)
+data.describe()
